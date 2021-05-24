93e9a4e7931d70dfe681eac3d113cc7ef46f0cfd
Shashank <s_shashank@outlook.com>
2021-05-24 23:09:07 +0530

    added extent report

--- src/main/java/com/qa/listeners/TestListener.java
+++ src/main/java/com/qa/listeners/TestListener.java
@@ -4,16 +4,22 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
 import com.qa.BaseTest;
+import com.qa.reports.ExtentReport;
 
 public class TestListener implements ITestListener {
 
@@ -27,6 +33,13 @@ public class TestListener implements ITestListener {
 
 		BaseTest base = new BaseTest();
 		File file = base.getDriver().getScreenshotAs(OutputType.FILE);
+		
+		byte[] encoded = null;
+		try {
+			encoded = Base64.encodeBase64(FileUtils.readFileToByteArray(file));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 
 		Map<String, String> params = new HashMap<String, String>();
 		params = result.getTestContext().getCurrentXmlTest().getAllParameters();
@@ -45,6 +58,28 @@ public class TestListener implements ITestListener {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		ExtentReport.getTest().fail("Test Failed",
+				MediaEntityBuilder.createScreenCaptureFromPath(completePath).build());
+		ExtentReport.getTest().fail("Test Failed",
+				MediaEntityBuilder.createScreenCaptureFromBase64String(new String(encoded, StandardCharsets.US_ASCII)).build());
+		ExtentReport.getTest().fail(result.getThrowable());
+	}
+	
+	public void onTestStart(ITestResult result) {
+		ExtentReport.startTest(result.getName(), result.getMethod().getDescription())
+		.assignCategory("Android_Pixel_3")
+		.assignAuthor("Shashank");		
 	}
 
+	public void onTestSuccess(ITestResult result) {
+		ExtentReport.getTest().log(Status.PASS, "Test Passed");
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		ExtentReport.getTest().log(Status.SKIP, "Test Skipped");	
+	}
+	
+	public void onFinish(ITestContext context) {
+		ExtentReport.getReporter().flush();		
+	}
 }
